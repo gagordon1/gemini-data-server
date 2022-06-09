c9585016712f0988ece992181410d077c86603e8
Garrett Gordon <gagordon@mit.edu>
2022-06-09 00:39:32 -0400

    bot functioning on Gemini

--- gemini_bot.py
+++ gemini_bot.py
@@ -2,11 +2,28 @@
 from dotenv import dotenv_values
 config = dotenv_values(".env")
 
+import requests
 import json
 from time import sleep
+import base64
+import hmac
+import hashlib
+import datetime, time
 
-price = 0.0
-states = ["bid", "waiting_to_fill", "ask", "waiting_to_fill"]
+BASE_URL = "https://api.gemini.com/v1"
+
+TICKER_INFO_ENDPOINT = "/pubticker/"
+
+NEW_ORDER_ENDPOINT = "/order/new"
+
+ORDER_STATUS_ENDPOINT = "/orders"
+
+PAST_TRADES_ENDPOINT = "/mytrades"
+
+CANCEL_ALL_ORDERS_ENDPOINT = "/order/cancel/all"
+
+
+
 
 class GeminiBot:
     def __init__(self, ticker, amountToBuy, amountToSell):
@@ -20,19 +37,185 @@ class GeminiBot:
         self.transactions = []
         self.timestamp = 0
         self.lastTradePrice = 0
+        self.end_condition_met = False
+        self.order_history = []
+        self.nonce = 0
 
     """
-    Check if an order has been placed
+    returns last price, lowest ask, highest bid for the object's ticker
+    """
+    def get_ticker_info(self):
+        print("Getting ticker info for {}".format(self.ticker))
+        try:
+            url = BASE_URL + TICKER_INFO_ENDPOINT + self.ticker
+            response = requests.get(url)
+            data = response.json()
+            return float(data["last"]), float(data["ask"]), float(data["bid"])
+        except Exception as e:
+            print(e)
+    """
+    Gets statuses of open orders or returns False on error
+    """
+    def get_order_statuses(self):
+        try:
+            gemini_api_key = config["GEMINI_TRADING_API_KEY"]
+            gemini_api_secret = config["GEMINI_TRADING_API_SECRET"].encode()
+
+            t = datetime.datetime.now()
+            payload_nonce =  str(int(time.mktime(t.timetuple())*1000))
+
+            payload = {
+                "nonce": payload_nonce,
+                "request": "/v1/orders"
+            }
+
+            url = BASE_URL + ORDER_STATUS_ENDPOINT
+            encoded_payload = json.dumps(payload).encode()
+            b64 = base64.b64encode(encoded_payload)
+            signature = hmac.new(gemini_api_secret, b64, hashlib.sha384).hexdigest()
+
+            request_headers = { 'Content-Type': "text/plain",
+                                'Content-Length': "0",
+                                'X-GEMINI-APIKEY': gemini_api_key,
+                                'X-GEMINI-PAYLOAD': b64,
+                                'X-GEMINI-SIGNATURE': signature,
+                                'Cache-Control': "no-cache" }
+            response = requests.post(url,
+                            data=None,
+                            headers=request_headers)
+
+            orders = response.json()
+            return orders
+        except Exception as e:
+            print(e)
+            return False
+
+    """
+    Gets statuses of closed orders or returns False on error
+    """
+    def get_past_trades(self):
+        print("getting past trades")
+        try:
+            gemini_api_key = config["GEMINI_TRADING_API_KEY"]
+            gemini_api_secret = config["GEMINI_TRADING_API_SECRET"].encode()
+
+            t = datetime.datetime.now()
+            payload_nonce =  str(int(time.mktime(t.timetuple())*1000) +1)
+
+            payload = {
+                "nonce": payload_nonce,
+                "request":  "/v1/mytrades",
+                "symbol" : self.ticker,
+                "limit_trades" : 10
+            }
+
+            url = BASE_URL + PAST_TRADES_ENDPOINT
+            encoded_payload = json.dumps(payload).encode()
+            b64 = base64.b64encode(encoded_payload)
+            signature = hmac.new(gemini_api_secret, b64, hashlib.sha384).hexdigest()
+
+            request_headers = { 'Content-Type': "text/plain",
+                                'Content-Length': "0",
+                                'X-GEMINI-APIKEY': gemini_api_key,
+                                'X-GEMINI-PAYLOAD': b64,
+                                'X-GEMINI-SIGNATURE': signature,
+                                'Cache-Control': "no-cache" }
+            response = requests.post(url,
+                            data=None,
+                            headers=request_headers)
+
+            orders = response.json()
+            return orders
+        except Exception as e:
+            print(e)
+            return False
+
+
+    """
+    Check if exactly one ticker in our order history is open
     If so, see how much of the order is left to be filled
-    """
-    def order_remaining(self):
-        pass
+    return amount_remaining, original_amount
+    otherwise
+    print an error message and return False
 
     """
-    If an order has yet to be filled cancel it
+    def get_order_status(self):
+        try:
+            orders = self.get_order_statuses()
+
+            if len(orders) == 0:
+                print("No orders open")
+                #CHECK IF ANY ORDERS IN ORDER HISTORY HAVE EXECUTED
+                recent_trades = self.get_past_trades()
+                for order in self.order_history:
+                    if float(order["amount_remaining"]) > 0:
+                        for o in recent_trades:
+                            if o["order_id"] == order["id"]:
+                                order["amount_remaining"] = 0.0
+                                print("trade has been executed")
+                                return 0.0, float(order["original_amount"])
+                return False
+            elif len(orders) == 1:
+                order = orders[0]
+                #check that order is in this instance's order history
+                found = False
+                for o in self.order_history:
+                    if o["id"] == order["id"]:
+                        found = True
+                if not found:
+                    return False
+                print("remaining_amount: {}".format(order["remaining_amount"]))
+                return float(order["remaining_amount"]), float(order["original_amount"])
+            else:
+                return False
+
+
+        except Exception as e:
+            print(e)
+
     """
-    def cancel_order(self):
-        pass
+    Cancel open orders
+    Returns True if successfully canceled false otherwise
+    """
+    def cancel_active_orders(self):
+        print("canceling orders")
+        try:
+            gemini_api_key = config["GEMINI_TRADING_API_KEY"]
+            gemini_api_secret = config["GEMINI_TRADING_API_SECRET"].encode()
+
+            t = datetime.datetime.now()
+            payload_nonce =  str(int(time.mktime(t.timetuple())*1000))
+
+            payload = {
+                "nonce": payload_nonce,
+                "request": "/v1/order/cancel/all"
+            }
+
+            url = BASE_URL + CANCEL_ALL_ORDERS_ENDPOINT
+            encoded_payload = json.dumps(payload).encode()
+            b64 = base64.b64encode(encoded_payload)
+            signature = hmac.new(gemini_api_secret, b64, hashlib.sha384).hexdigest()
+
+            request_headers = { 'Content-Type': "text/plain",
+                                'Content-Length': "0",
+                                'X-GEMINI-APIKEY': gemini_api_key,
+                                'X-GEMINI-PAYLOAD': b64,
+                                'X-GEMINI-SIGNATURE': signature,
+                                'Cache-Control': "no-cache" }
+            response = requests.post(url,
+                            data=None,
+                            headers=request_headers)
+
+            result = response.json()
+            if result["result"] == "ok":
+                for order in self.order_history:
+                    if not order["cancelled"]:
+                        order["cancelled"] = True
+                return True
+            return False
+        except Exception as e:
+            print(e)
+            return False
 
     """
     ticker : str
@@ -42,12 +225,78 @@ class GeminiBot:
     returns boolean : true if successful, false otherwise
     """
     def maker_or_cancel_order(self, type, price, amount):
-        print("Limit or cancel {} order placed: {}{} at ${}").format(
-                type, amount, self.ticker, price)
+        print("Placing limit {} order".format(type))
+        price = round(price, 2)
+
+        try:
+
+            gemini_api_key = config["GEMINI_TRADING_API_KEY"]
+            gemini_api_secret = config["GEMINI_TRADING_API_SECRET"].encode()
+
+            t = datetime.datetime.now()
+            payload_nonce =  str(int(time.mktime(t.timetuple())*1000))
+
+            side = "buy"
+            if type == "ask":
+                side = "sell"
+
+
+            payload = {
+               "request": "/v1/order/new",
+                "nonce": payload_nonce,
+                "symbol": self.ticker,
+                "amount": str(amount),
+                "price": str(price),
+                "side": side,
+                "type": "exchange limit",
+                "options": ["maker-or-cancel"]
+            }
+
+            url = BASE_URL + NEW_ORDER_ENDPOINT
+            encoded_payload = json.dumps(payload).encode()
+            b64 = base64.b64encode(encoded_payload)
+            signature = hmac.new(gemini_api_secret, b64, hashlib.sha384).hexdigest()
+
+            request_headers = { 'Content-Type': "text/plain",
+                                'Content-Length': "0",
+                                'X-GEMINI-APIKEY': gemini_api_key,
+                                'X-GEMINI-PAYLOAD': b64,
+                                'X-GEMINI-SIGNATURE': signature,
+                                'Cache-Control': "no-cache" }
+            response = requests.post(url,
+                            data=None,
+                            headers=request_headers)
+
+            new_order = response.json()
+            if "is_live" in new_order and new_order["is_live"]:
+                print("Limit or cancel {} order placed: {}{} at ${}".format(
+                        type, amount, self.ticker, price))
+                self.order_history.append(
+                    {
+                        "id" : new_order["order_id"],
+                        "symbol" : new_order["symbol"],
+                        "side" : new_order["side"],
+                        "price" : new_order["price"],
+                        "timestamp" : new_order["timestamp"],
+                        "original_amount" :new_order["original_amount"],
+                        "amount_remaining" : new_order["remaining_amount"],
+                        "cancelled" : False
+                    }
+                )
+                print(self.order_history)
+                return True
+            else:
+                print(new_order)
+                return False
+        except Exception as e:
+            print(e)
+            return False
+
+
+
+
 
     """
-    ws : WebSocketApp
-    message : json string
     Algorithm:
         0 (decide to buy or sell):
             if (amountToBuy > amountToSell)
@@ -63,6 +312,7 @@ class GeminiBot:
                 decrement amount to buy
                 move to state 0
             if (last_price > bid_order_price * (1 + margin))
+                cancel order
                 decrement amount to buy if partially filled
                 move to state 0
         3 (sell)
@@ -75,34 +325,79 @@ class GeminiBot:
                 decrement amount to sell
                 move to state 0
             if (last_price < bid_order_price * (1-margin))
+                cancel order
                 decrement amonunt to sell if partially filled
                 move to state 0
 
     """
-    def strategy1(self, ws, message):
+    def strategy1(self):
         #BTC SPREAD IS ~ .0002 * price
 
+        last_price, lowest_ask, highest_bid = self.get_ticker_info()
+        trade_size = .00005
+        margin = .0001
         # UPDATE BOT STATE
         if self.state == 0:
-            pass
+            print("Deciding to buy or sell next")
+            if self.amountToBuy > self.amountToSell:
+                self.state = 1
+            else:
+                self.state = 3
 
         elif self.state == 1:
             print("Making buy order...")
-            pass
+            self.bid_order_price = last_price*(1 - margin)
+            order_success =  self.maker_or_cancel_order("bid", self.bid_order_price, trade_size)
+            if order_success:
+                self.state = 2
+            else:
+                print("buy order failed")
 
         elif self.state == 2:
             print("Waiting for buy order to fill...")
+            amount_remaining, original_amount = self.get_order_status()
+            if amount_remaining == 0:
+                self.amountToBuy -= original_amount
+                self.state = 0
+            elif last_price > self.bid_order_price * (1 + margin): #market has moved the other way
+                success = self.cancel_active_orders()
+                if success:
+                    self.amountToBuy -= (original_amount - amount_remaining) #amount bought
+                    self.state = 0
+                else:
+                    print("cancel order failed")
 
         elif self.state == 3:
             print("Making sell order")
+            self.sell_order_price = last_price*(1 + margin)
+            order_success = self.maker_or_cancel_order("ask", self.sell_order_price, trade_size)
+            if order_success:
+                self.state = 4
+            else:
+                print("sell order failed")
 
         elif self.state == 4:
             print("Waiting for sell to fill...")
+            amount_remaining, original_amount = self.get_order_status()
+            if amount_remaining == 0:
+                self.amountToSell -= original_amount
+                self.state = 0
+            elif last_price < self.sell_order_price * (1 - margin):
+                success = self.cancel_active_orders()
+                if success:
+                    self.amountToSell -= (original_amount - amount_remaining) #amount successfully sold
+                    self.state = 0
+                else:
+                    print("cancel order failed")
 
+        if self.amountToSell == 0 and self.amountToBuy == 0:
+            self.end_condition_met = True
 
-        elif self.state == 5:
-            print("Safe debugging state...")
-            #safe debugging state
+    def run(self):
+        while self.end_condition_met == False:
+            sleep(1)
+            self.strategy1()
 
 if __name__ == '__main__':
     g = GeminiBot("btcusd", .0005, .0005)
+    g.run()
--- gemini_bot_test.py
+++ gemini_bot_test.py
@@ -1,7 +1,26 @@
 from gemini_bot import GeminiBot
+from time import sleep
 
 ticker = "btcusd"
-amountToBuy = .0005 #$15
-amountToSell = .0005 #$15
+amountToBuy = .0003 #$15
+amountToSell = .0003 #$15
 g = GeminiBot(ticker, amountToBuy, amountToSell)
-g.maker_or_cancel_order("bid", )
+
+
+last_price, lowest_ask, highest_bid = g.get_ticker_info()
+print("last price: {}, lowest ask: {}, highest bid: {}".format(last_price, lowest_ask, highest_bid))
+
+
+# success = g.maker_or_cancel_order("ask", 60000, amountToBuy) # make an ask at 60000
+# sleep(1)
+# # success = g.maker_or_cancel_order("bid", 20, amountToBuy) # make an bid at 20
+# # sleep(1)
+# status = g.get_order_status()
+# print(status)
+# sleep(1)
+# #
+# print(g.order_history)
+# g.cancel_active_orders()
+# print(g.order_history)
+# sleep(1)
+# status = g.get_order_status()
